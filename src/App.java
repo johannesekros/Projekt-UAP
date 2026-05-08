723815b5468d1465417810559a6c706b7ff42ff1
AndreasState <andreas.state@icloud.com>
2026-05-08 15:07:13 +0200

    Moved everything to src folder and started doing App

--- src/App.java
+++ src/App.java
@@ -1,7 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class App {
+    static int[] readFile(String path) throws Exception {
+            
+            ArrayList<Integer> list = new ArrayList<>();
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(Integer.parseInt(line));
+            }
+            br.close();
+            return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+        }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("This is a Java application.");
+        
+        BinarySearchTree bst = new BinarySearchTree();
+        
+
+        long start = System.nanoTime();
+
+        int[] data = readFile("randomNumbers/DataSmall.txt");
+
+        for (int num : data){
+            bst.add(num);
+        }
+
+        long elapsed = System.nanoTime() - start;
+
+        System.out.println("BST" + "," + "Small" + "," + "insert" + "," + elapsed);
     }
+
+
 }
 
